3df9a89c8ceee61fd8a0c77ddf100e309d4b553e
Luis J. Montes <luisjavm3@gmail.com>
2022-09-06 15:26:42 -0500

    Create Entity entity

---
+++ Cobros.API/Entities/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace Cobros.API.Entities
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeleteAt { get; init; }
+        public bool IsDeleted => DeleteAt != null ? true : false;
+    }
+}
