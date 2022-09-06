2e216aac8c001183c0ef684f72b60f076aa392fe
Luis J. Montes <luisjavm3@gmail.com>
2022-09-06 16:44:55 -0500

    Create Gender and User entities

---
+++ Cobros.API/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Cobros.API.Entities
+{
+    public class User : Entity
+    {
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
