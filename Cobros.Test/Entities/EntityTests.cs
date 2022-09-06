2cb6e9c4934efbab7e0a788e0a8a3c0415632c54
Luis J. Montes <luisjavm3@gmail.com>
2022-09-06 16:36:08 -0500

    Test Entity

---
+++ Cobros.Test/Entities/EntityTests.cs
@@ -0,0 +1,35 @@
+﻿using Cobros.API.Entities;
+using FluentAssertions;
+
+namespace Cobros.Test.Entities
+{
+    public class EntityTests
+    {
+        [Fact]
+        public void IsDeleted_IsFalse_WhenDeletedAtIsNull()
+        {
+            // Arrange
+            var entity = new Entity();
+
+            // Act
+            var sut = entity.IsDeleted;
+
+            // Assert
+            sut.Should().BeFalse();
+        }        
+        
+        [Fact]
+        public void IsDeleted_IsTrue_WhenDeletedAtIsADate()
+        {
+            // Arrange
+            var entity = new Entity();
+            entity.DeletedAt = DateTime.UtcNow;
+
+            // Act
+            var sut = entity.IsDeleted;
+
+            // Assert
+            sut.Should().BeTrue();
+        }
+    }
+}
