37a693d699eb2fc13a060eea861f99718c806e5b
Luis J. Montes <luisjavm3@gmail.com>
2022-09-08 14:48:50 -0500

    Create BadRequestException

---
+++ Cobros.API/Core/Model/Exceptions/BadRequestException.cs
@@ -0,0 +1,15 @@
+﻿namespace Cobros.API.Core.Model.Exceptions
+{
+    public class BadRequestException:Exception
+    {
+        public BadRequestException()
+        {
+                
+        }
+
+        public BadRequestException(string message):base(message)
+        {
+
+        }
+    }
+}
