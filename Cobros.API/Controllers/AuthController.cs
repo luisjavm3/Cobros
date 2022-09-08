3387bacd9f664ae16084186acec53121f2a7ec81
Luis J. Montes <luisjavm3@gmail.com>
2022-09-08 14:47:21 -0500

    Create AuthController

---
+++ Cobros.API/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cobros.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController:ControllerBase
+    {
+        [HttpPost("Register")]
+        public Task<IActionResult> Register()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
