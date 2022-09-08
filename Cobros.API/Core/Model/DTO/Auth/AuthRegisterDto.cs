653ad9157ad10000b2274a23d516cea51b52828f
Luis J. Montes <luisjavm3@gmail.com>
2022-09-08 14:48:37 -0500

    Create AuthRegisterDto

---
+++ Cobros.API/Core/Model/DTO/Auth/AuthRegisterDto.cs
@@ -0,0 +1,27 @@
+﻿using Cobros.API.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cobros.API.Core.Model.DTO.Auth
+{
+    public class AuthRegisterDto
+    {
+        [Required]
+        [MaxLength(255)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string LastName { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Password { get; set; }
+    }
+}
