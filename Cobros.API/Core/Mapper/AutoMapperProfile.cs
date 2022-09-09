c2dfafb4b6c5d3dee498dc1bc30ab9a0148316d7
Luis J. Montes <luisjavm3@gmail.com>
2022-09-08 22:40:20 -0500

    Setup AutoMapper

---
+++ Cobros.API/Core/Mapper/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Cobros.API.Core.Model.DTO.Auth;
+using Cobros.API.Entities;
+
+namespace Cobros.API.Core.Mapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<AuthRegisterDto, User>();
+        }
+    }
+}
