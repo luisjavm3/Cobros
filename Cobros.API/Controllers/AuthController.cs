5c52a1946d099338dc6d6dfe39e324d903cc58ff
Luis J. Montes <luisjavm3@gmail.com>
2022-09-08 22:40:47 -0500

    Implement Register

--- Cobros.API/Controllers/AuthController.cs
+++ Cobros.API/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cobros.API.Core.Business.Interfaces;
+using Cobros.API.Core.Model.DTO.Auth;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cobros.API.Controllers
 {
@@ -6,10 +8,25 @@ namespace Cobros.API.Controllers
     [Route("[controller]")]
     public class AuthController:ControllerBase
     {
-        [HttpPost("Register")]
-        public Task<IActionResult> Register()
+        private readonly IAuthBusiness _authBusiness;
+
+        public AuthController(IAuthBusiness authBusiness)
         {
-            throw new NotImplementedException();
+            _authBusiness = authBusiness;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult> Register(AuthRegisterDto authRegisterDto)
+        {
+            try
+            {
+                await _authBusiness.Register(authRegisterDto);
+                return Ok();
+            }
+            catch (Exception er)
+            {
+                return BadRequest(er.Message);
+            }
         }
     }
 }
--- Cobros.API/Core/Business/AuthBusiness.cs
+++ Cobros.API/Core/Business/AuthBusiness.cs
@@ -1,6 +1,8 @@
-﻿using Cobros.API.Core.Business.Interfaces;
+﻿using AutoMapper;
+using Cobros.API.Core.Business.Interfaces;
 using Cobros.API.Core.Model.DTO.Auth;
 using Cobros.API.Core.Model.Exceptions;
+using Cobros.API.Entities;
 using Cobros.API.Repositories.Interfaces;
 
 namespace Cobros.API.Core.Business
@@ -8,21 +10,26 @@ namespace Cobros.API.Core.Business
     public class AuthBusiness : IAuthBusiness
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public AuthBusiness(IUnitOfWork unitOfWork)
+        public AuthBusiness(IUnitOfWork unitOfWork, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
 
         public async Task Register(AuthRegisterDto authRegisterDto)
         {
             var existing = await _unitOfWork.Users.GetByUsernameAsync(authRegisterDto.Username);
 
-            if (existing == null)
+            if (existing != null)
                 throw new BadRequestException($"Username {authRegisterDto.Username} already exists.");
 
+            var user = _mapper.Map<User>(authRegisterDto);
+            user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(authRegisterDto.Password);
 
-            throw new NotImplementedException();
+            await _unitOfWork.Users.InsertAsync(user);
+            await _unitOfWork.CompleteAsync();
         }
     }
 }
