6e12db16307d5e892d33c683e7d0a24f12fc2c3c
Luis J. Montes <luisjavm3@gmail.com>
2022-09-09 10:41:19 -0500

    Test Register when username is repeated

--- Cobros.Test/Core/Business/AuthBusinessTests.cs
+++ Cobros.Test/Core/Business/AuthBusinessTests.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Cobros.API.Core.Business;
 using Cobros.API.Core.Model.DTO.Auth;
+using Cobros.API.Core.Model.Exceptions;
 using Cobros.API.Entities;
 using Cobros.API.Repositories.Interfaces;
 using FluentAssertions;
@@ -40,5 +41,33 @@ namespace Cobros.Test.Core.Business
                 .Should()
                 .NotThrowAsync();
         }
+
+        [Fact]
+        public async void Register_ThrowsBadRequestException_WhenUsernameIsRepeated()
+        {
+            // Arrange
+            var unitOfWork = new Mock<IUnitOfWork>();
+            var userRepository = new Mock<IUserRepository>();
+            var mapper = new Mock<IMapper>();
+            var repeatedUsername = "repeated_username";
+
+            userRepository
+                .Setup(x => x.GetByUsernameAsync(repeatedUsername))
+                .Returns(Task.FromResult<User>(new User { Username = repeatedUsername }));
+
+            unitOfWork
+                .Setup(x => x.Users)
+                .Returns(() => userRepository.Object);
+
+            // SUT - Kind of Act.
+            var sut = new AuthBusiness(unitOfWork.Object, mapper.Object);
+
+            await sut
+                .Awaiting(x => x.Register(new AuthRegisterDto { Username = repeatedUsername }))
+                .Should()
+                .ThrowAsync<AppException>();
+        }
+
+        //
     }
 }
