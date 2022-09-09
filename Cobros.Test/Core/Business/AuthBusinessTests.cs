e9de4879ca4cbe433a113ed0702bd82aa3375882
Luis J. Montes <luisjavm3@gmail.com>
2022-09-08 22:42:03 -0500

    Test Register when username is not repeated

---
+++ Cobros.Test/Core/Business/AuthBusinessTests.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Cobros.API.Core.Business;
+using Cobros.API.Core.Model.DTO.Auth;
+using Cobros.API.Entities;
+using Cobros.API.Repositories.Interfaces;
+using FluentAssertions;
+using Moq;
+
+namespace Cobros.Test.Core.Business
+{
+    public class AuthBusinessTests
+    {
+        [Fact]
+        public async void Register_IsSuccessful_WhenUsernameIsNotRepeated()
+        {
+            // Arrange
+            var unitOfWork = new Mock<IUnitOfWork>();
+            var userRepository = new Mock<IUserRepository>();
+            var mapper = new Mock<IMapper>();
+            var notRepeatedUsername = "not_repeated_username";
+
+            userRepository
+                .Setup(x => x.GetByUsernameAsync(notRepeatedUsername))
+                .Returns(Task.FromResult<User>(null));
+
+            unitOfWork
+                .Setup(x => x.Users)
+                .Returns(userRepository.Object);
+
+            mapper
+                .Setup(x => x.Map<User>(It.IsAny<AuthRegisterDto>()))
+                .Returns(new User { Username = notRepeatedUsername, Name = "name", LastName = "lastname", Gender = Gender.FEMALE });
+
+            // System under test - kinda Act
+            var sut = new AuthBusiness(unitOfWork.Object, mapper.Object);
+
+            // Assert
+            await sut
+                .Awaiting(x => x.Register(new AuthRegisterDto { Username = notRepeatedUsername, Password = "123456", Name = "name", LastName = "lastname", Gender = Gender.FEMALE }))
+                .Should()
+                .NotThrowAsync();
+        }
+    }
+}
