90795b20de1bee8a31d35eb9d73052b159f18432
Luis J. Montes <luisjavm3@gmail.com>
2022-09-09 10:57:02 -0500

    Implement AuthLoginDto

--- Cobros.API/Core/Model/DTO/Auth/AuthLoginDto.cs
+++ Cobros.API/Core/Model/DTO/Auth/AuthLoginDto.cs
@@ -1,6 +1,15 @@
-﻿namespace Cobros.API.Core.Model.DTO.Auth
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cobros.API.Core.Model.DTO.Auth
 {
     public class AuthLoginDto
     {
+        [Required(ErrorMessage ="Username is required.")]
+        [MaxLength(255, ErrorMessage ="Username must have less than or equal to 255 characters.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage ="Password is required.")]
+        [MaxLength(255, ErrorMessage ="Password must have less than or equal to 255 characters.")]
+        public string Password { get; set; }
     }
 }
