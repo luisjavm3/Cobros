3dfdea66f615b1d15a54695549b5994d0bb1ae76
Luis J. Montes <luisjavm3@gmail.com>
2022-09-09 20:38:25 -0500

    Modify Login

--- Cobros.API/Core/Business/AuthBusiness.cs
+++ Cobros.API/Core/Business/AuthBusiness.cs
@@ -1,9 +1,16 @@
 ﻿using AutoMapper;
 using Cobros.API.Core.Business.Interfaces;
+using Cobros.API.Core.Helper;
 using Cobros.API.Core.Model.DTO.Auth;
 using Cobros.API.Core.Model.Exceptions;
 using Cobros.API.Entities;
 using Cobros.API.Repositories.Interfaces;
+using Cobros.API.Settings;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography.Xml;
+using System.Text;
 
 namespace Cobros.API.Core.Business
 {
@@ -11,11 +18,13 @@ namespace Cobros.API.Core.Business
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
+        private readonly JwtSettings _jwtSettings;
 
-        public AuthBusiness(IUnitOfWork unitOfWork, IMapper mapper)
+        public AuthBusiness(IUnitOfWork unitOfWork, IMapper mapper, IConfiguration configuration)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
+            _jwtSettings = configuration.GetSection(nameof(JwtSettings)).Get<JwtSettings>();
         }
 
         public async Task Register(AuthRegisterDto authRegisterDto)
@@ -32,9 +41,60 @@ namespace Cobros.API.Core.Business
             await _unitOfWork.CompleteAsync();
         }
 
-        public Task<string> Login(AuthLoginDto authLoginDto)
+        public async Task<string> Login(AuthLoginDto authLoginDto)
         {
-            throw new NotImplementedException();
+            var existing = await _unitOfWork.Users.GetByUsernameAsync(authLoginDto.Username);
+
+            // throw an exception if username does not exist.
+            if (existing == null)
+                throw new AppException("Wrong credentials.");
+
+            // throw an exception if user was deleted.
+            if (existing.IsDeleted)
+                throw new AppException($"Wrong credentials.");
+
+            // throw an exception if password is incorrect.
+            if (!BCrypt.Net.BCrypt.Verify(authLoginDto.Password, existing.PasswordHash))
+                throw new AppException("Wrong credentials.");
+
+            try
+            {
+                _unitOfWork.BeginTransaccion();
+
+                // Remove all Refresh Tokens from a user
+                await RefreshTokenHelper.RemoveUserRefreshTokens(existing.Id, _unitOfWork);
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return GetToken(existing.Id);
+        }
+
+        private string GetToken(int userId)
+        {
+            var encodedKey = Encoding.UTF8.GetBytes(_jwtSettings.Key);
+            var signingKey = new SymmetricSecurityKey(encodedKey);
+            var signingCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha512Signature);
+            var claims = new List<Claim> {
+                new Claim(type: ClaimTypes.NameIdentifier, value: userId.ToString())
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                SigningCredentials = signingCredentials,
+                Expires = DateTime.UtcNow.AddMinutes(_jwtSettings.LifetimeMinutes)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
