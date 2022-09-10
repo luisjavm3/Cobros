5e3f54301d012ea63b9e5eee2bddfe45d814f5d2
Luis J. Montes <luisjavm3@gmail.com>
2022-09-09 20:37:53 -0500

    Create RefreshToken entity

---
+++ Cobros.API/Entities/RefreshToken.cs
@@ -0,0 +1,13 @@
+﻿namespace Cobros.API.Entities
+{
+    public class RefreshToken : Entity
+    {
+        public string Value { get; set; }
+        public string FirstRefreshTokenSession { get; set; }
+        public string PreviousRefeshToken { get; set; }
+        public string ReasonRevoked { get; set; }
+        public bool IsRevoked => ReasonRevoked != null ? true : false;
+        public bool IsExpired => CreatedAt.AddDays(1) <= DateTime.UtcNow;
+        public User User { get; set; }
+    }
+}
