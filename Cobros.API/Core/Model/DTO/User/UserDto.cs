2fc0d30429d45eccf8c26ed5b3da9f8a5626d048
Luis J. Montes <luisjavm3@gmail.com>
2022-09-13 15:53:16 -0500

    Create UserDto

---
+++ Cobros.API/Core/Model/DTO/User/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using Cobros.API.Entities;
+
+namespace Cobros.API.Core.Model.DTO.User
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public Role Role { get; set; }
+    }
+}
