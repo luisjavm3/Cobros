7bb9cc38b3f4568b852a80b727a87352adf9bc96
Luis J. Montes <luisjavm3@gmail.com>
2022-09-16 16:47:22 -0500

    Create test for data population

---
+++ Cobros.Test/DataAccess/ApplicationDbContextTest.cs
@@ -0,0 +1,34 @@
+﻿using Cobros.API.Entities;
+using FluentAssertions;
+
+namespace Cobros.Test.DataAccess
+{
+    public class ApplicationDbContextTest
+    {
+        [Fact]
+        public async void ApplicationDbContext_WasPopulatedWithTestData()
+        {
+            // Arrange
+            var helper = new TestHelpers();
+            var unitOfWork = helper.GetUnitOfWork();
+
+            // Act
+            var result = await unitOfWork.Users.GetByIdAsync(1);
+
+            // Assert
+            result
+                .Should()
+                .NotBeNull();
+
+            result
+                .Should()
+                .BeOfType<User>();
+
+            result
+                .Username
+                .Should()
+                .Be("User1");
+        }
+
+    }
+}
