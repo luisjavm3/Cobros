23528acaa31ece14c915699b8c866ef63b1be737
Luis J. Montes <luisjavm3@gmail.com>
2022-09-16 16:46:35 -0500

    Create TestHelpers class

---
+++ Cobros.Test/TestHelpers.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Cobros.API.Core.Mapper;
+using Cobros.API.DataAccess;
+using Cobros.API.Repositories;
+using Cobros.API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cobros.Test
+{
+    public class TestHelpers
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public TestHelpers()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseInMemoryDatabase(databaseName: "testDb");
+
+            _applicationDbContext = new ApplicationDbContext(optionsBuilder.Options);
+
+            _applicationDbContext.Database.EnsureDeleted();
+            _applicationDbContext.Database.EnsureCreated();
+        }
+
+        public IUnitOfWork GetUnitOfWork()
+        {
+            return new UnitOfWork(_applicationDbContext);
+        }
+
+        public IMapper GetAutoMapperInstance()
+        {
+            var mapperConfiguration = new MapperConfiguration(options => options.AddProfile(new AutoMapperProfile()));
+            return new Mapper(mapperConfiguration);
+        }
+    }
+}
