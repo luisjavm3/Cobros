515a4b566d1659d33c4ad2709c40cf6b068d9f4d
Luis J. Montes <luisjavm3@gmail.com>
2022-09-20 15:10:32 -0500

    Setup ApplicationDbContext

--- Cobros.API/DataAccess/ApplicationDbContext.cs
+++ Cobros.API/DataAccess/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Cobros.API.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
 
 namespace Cobros.API.DataAccess
 {
@@ -8,21 +9,43 @@ namespace Cobros.API.DataAccess
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {}
 
-        public DbSet<User> Users { get; set; }
         public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+        // Person entities
+        public DbSet<User> Users { get; set; }
+        public DbSet<DebtCollector> DebtCollectors { get; set; }
+        public DbSet<Customer> Customers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder
+                .Entity<RefreshToken>()
+                .HasIndex(x => x.Value)
+                .IsUnique();
+
+            modelBuilder
+                .Entity<Person>()
+                .ToTable("Persons")
+                .HasDiscriminator<int>("PersonType")
+                .HasValue<User>(1)
+                .HasValue<DebtCollector>(2)
+                .HasValue<Customer>(3);
+
+            modelBuilder
+                .Entity<Person>()
+                .HasIndex(x => x.NationalID)
+                .IsUnique();
+
             modelBuilder
                 .Entity<User>()
                 .HasIndex(x => x.Username)
                 .IsUnique();
 
             modelBuilder
-                .Entity<RefreshToken>()
-                .HasIndex(x => x.Value)
+                .Entity<Loan>()
+                .HasIndex(x => new { x.CobroId, x.RoutePosition })
                 .IsUnique();
-                
+
             modelBuilder.SeedDB();
         }
     }
