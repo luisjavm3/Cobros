22450203e9a20d2a49c3c5717f13fcd49be482c0
Luis J. Montes <luisjavm3@gmail.com>
2022-09-21 18:56:25 -0500

    Populate Users with NationalID

--- Cobros.API/DataAccess/SeedingData.cs
+++ Cobros.API/DataAccess/SeedingData.cs
@@ -10,13 +10,13 @@ namespace Cobros.API.DataAccess
             modelBuilder
                 .Entity<User>()
                 .HasData(
-                    new User { Id = 1, Name="User1", LastName="User_Lastname1",Username="User1",Gender=Gender.MALE, Role = Role.USER, PasswordHash=BCrypt.Net.BCrypt.HashPassword("123456")},
-                    new User { Id = 2, Name = "User2", LastName="User_Lastname2",Username="User2",Gender=Gender.FEMALE, Role = Role.USER, PasswordHash = BCrypt.Net.BCrypt.HashPassword("123456")},
-                    new User { Id = 3, Name = "User3", LastName="User_Lastname3",Username="User3",Gender=Gender.MALE, Role = Role.USER, PasswordHash = BCrypt.Net.BCrypt.HashPassword("123456")},
-                    new User { Id = 4, Name = "User4", LastName="User_Lastname4",Username="User4",Gender=Gender.FEMALE, Role = Role.USER, PasswordHash = BCrypt.Net.BCrypt.HashPassword("123456")},
-                    new User { Id = 5, Name = "User5", LastName="User_Lastname5",Username="User5",Gender=Gender.MALE, Role = Role.USER, PasswordHash = BCrypt.Net.BCrypt.HashPassword("123456")},
-                    new User { Id = 6, Name = "Admin1", LastName="Admin_Lastname1",Username="Admin1",Gender=Gender.FEMALE, Role = Role.ADMIN, PasswordHash = BCrypt.Net.BCrypt.HashPassword("123456")},
-                    new User { Id = 7, Name = "Admin2", LastName="Admin_Lastname2",Username="Admin2",Gender=Gender.FEMALE, Role = Role.ADMIN, PasswordHash = BCrypt.Net.BCrypt.HashPassword("123456")}
+                    new User { Id = 1, NationalID= "23456", Name="User1", LastName="User_Lastname1",Username="User1",Gender=Gender.MALE, Role = Role.USER, PasswordHash=BCrypt.Net.BCrypt.HashPassword("123456")},
+                    new User { Id = 2, NationalID = "23457", Name = "User2", LastName="User_Lastname2",Username="User2",Gender=Gender.FEMALE, Role = Role.USER, PasswordHash = BCrypt.Net.BCrypt.HashPassword("123456")},
+                    new User { Id = 3, NationalID = "23458", Name = "User3", LastName="User_Lastname3",Username="User3",Gender=Gender.MALE, Role = Role.USER, PasswordHash = BCrypt.Net.BCrypt.HashPassword("123456")},
+                    new User { Id = 4, NationalID = "23459", Name = "User4", LastName="User_Lastname4",Username="User4",Gender=Gender.FEMALE, Role = Role.USER, PasswordHash = BCrypt.Net.BCrypt.HashPassword("123456")},
+                    new User { Id = 5, NationalID = "234560", Name = "User5", LastName="User_Lastname5",Username="User5",Gender=Gender.MALE, Role = Role.USER, PasswordHash = BCrypt.Net.BCrypt.HashPassword("123456")},
+                    new User { Id = 6, NationalID = "234561", Name = "Admin1", LastName="Admin_Lastname1",Username="Admin1",Gender=Gender.FEMALE, Role = Role.ADMIN, PasswordHash = BCrypt.Net.BCrypt.HashPassword("123456")},
+                    new User { Id = 7, NationalID = "234562", Name = "Admin2", LastName="Admin_Lastname2",Username="Admin2",Gender=Gender.FEMALE, Role = Role.ADMIN, PasswordHash = BCrypt.Net.BCrypt.HashPassword("123456")}
                 );
 
             modelBuilder
