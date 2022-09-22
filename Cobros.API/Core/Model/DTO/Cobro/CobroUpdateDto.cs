5b8951bcf0541d252d06e33f18a0019de6a92eb6
Luis J. Montes <luisjavm3@gmail.com>
2022-09-22 18:18:15 -0500

    Create CobroUpdateDto

---
+++ Cobros.API/Core/Model/DTO/Cobro/CobroUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using Cobros.API.Entities;
+
+namespace Cobros.API.Core.Model.DTO.Cobro
+{
+    public class CobroUpdateDto
+    {
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public int DebtCollectorId { get; set; }
+    }
+}
