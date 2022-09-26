4e741a4505613c39609043414d5f2efb50f9b774
Luis J. Montes <luisjavm3@gmail.com>
2022-09-26 11:41:07 -0500

    Modify User entity on its relationship with Cobro - OneToOne to OneToMany

--- Cobros.API/Entities/User.cs
+++ Cobros.API/Entities/User.cs
@@ -7,6 +7,6 @@ namespace Cobros.API.Entities
         public string Username { get; set; }
         public Role Role { get; set; } = Role.USER;
         public string PasswordHash { get; set; }
-        public Cobro Cobro { get; set; }
+        public IEnumerable<Cobro> Cobro { get; set; }
     }
 }
