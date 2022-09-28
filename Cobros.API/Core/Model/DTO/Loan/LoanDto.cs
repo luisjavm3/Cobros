489cf3411dfd558c805e2fab1af27bfe02835785
Luis J. Montes <luisjavm3@gmail.com>
2022-09-28 11:53:26 -0500

    Modify Loan entity and create LoanDto

---
+++ Cobros.API/Core/Model/DTO/Loan/LoanDto.cs
@@ -0,0 +1,18 @@
+﻿using Cobros.API.Entities;
+
+namespace Cobros.API.Core.Model.DTO.Loan
+{
+    public class LoanDto
+    {
+        public int Id { get; set; }
+        public int CobroId { get; set; }
+        public int CustomerId { get; set; }
+        public int Value { get; set; }
+        public int LoanInterest { get; set; }
+        public int RoutePosition { get; set; }
+        public int Balance { get; set; }
+        public double Total { get; set; }
+        public int TotalPaid { get; set; }
+
+    }
+}
--- Cobros.API/Entities/Loan.cs
+++ Cobros.API/Entities/Loan.cs
@@ -14,8 +14,5 @@
         public int Balance { get; set; }
 
         public IEnumerable<PartialPayment> PartialPayments { get; set; }
-
-        public double Total => Value * (1 + LoanInterest / (double)100);
-        public int TotalPaid => PartialPayments.Aggregate(0, (acc, x) => acc + x.Value);
     }
 }
