a415c82af4d06e2cfef0c31780089ac8de081899
Luis J. Montes <luisjavm3@gmail.com>
2022-09-29 08:54:06 -0500

    Modify Cobro name to Cobros on User entity

--- Cobros.API/Entities/User.cs
+++ Cobros.API/Entities/User.cs
@@ -1,12 +1,10 @@
-﻿using System.Security.Cryptography;
-
-namespace Cobros.API.Entities
+﻿namespace Cobros.API.Entities
 {
     public class User : Person
     {
         public string Username { get; set; }
         public Role Role { get; set; } = Role.USER;
         public string PasswordHash { get; set; }
-        public IEnumerable<Cobro> Cobro { get; set; }
+        public IEnumerable<Cobro> Cobros { get; set; }
     }
 }
