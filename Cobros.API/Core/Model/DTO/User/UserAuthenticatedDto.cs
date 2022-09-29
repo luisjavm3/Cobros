9be94235632d2055455242cc43fed2d35f305c1e
Luis J. Montes <luisjavm3@gmail.com>
2022-09-29 08:52:45 -0500

    Create UserAuthenticatedDto

---
+++ Cobros.API/Core/Model/DTO/User/UserAuthenticatedDto.cs
@@ -0,0 +1,12 @@
+﻿
+using Cobros.API.Entities;
+
+namespace Cobros.API.Core.Model.DTO.User
+{
+    public class UserAuthenticatedDto
+    {
+        public string Username { get; set; }
+        public Role Role { get; set; }
+        public IEnumerable<Cobros.API.Entities.Cobro> Cobros { get; set; }
+    }
+}
