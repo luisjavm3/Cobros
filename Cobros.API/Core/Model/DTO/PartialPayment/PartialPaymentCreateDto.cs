737f72bbf835b655e69089bf2f5c6947765008d2
Luis J. Montes <luisjavm3@gmail.com>
2022-09-30 16:34:26 -0500

    Create PartialPaymentCreateDto

---
+++ Cobros.API/Core/Model/DTO/PartialPayment/PartialPaymentCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cobros.API.Core.Model.DTO.PartialPayment
+{
+    public class PartialPaymentCreateDto
+    {
+        [Required(ErrorMessage ="Value is required.")]
+        [Range(1, int.MaxValue, ErrorMessage ="Value must be greater than 0.")]
+        public int Value { get; set; }
+    }
+}
