c04a79874b66918a3d9119a62785091b4406e774
Luis J. Montes <luisjavm3@gmail.com>
2022-09-30 16:34:59 -0500

    Create PartialPaymentController

---
+++ Cobros.API/Controllers/PartialPaymentController.cs
@@ -0,0 +1,26 @@
+﻿using Cobros.API.Core.Business.Interfaces;
+using Cobros.API.Core.Model.Authorize;
+using Cobros.API.Core.Model.DTO.PartialPayment;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cobros.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public class PartialPaymentController:ControllerBase
+    {
+        private readonly IPartialPaymentBusiness _partialPaymentBusiness;
+
+        public PartialPaymentController(IPartialPaymentBusiness partialPaymentBusiness)
+        {
+            _partialPaymentBusiness = partialPaymentBusiness;
+        }
+
+        [HttpPost("Loans/{loanId}/PartialPayments")]
+        public async Task<IActionResult> Insert(int loanId, PartialPaymentCreateDto partialPaymentCreateDto)
+        {
+            await _partialPaymentBusiness.InsertPartialPayment(loanId, partialPaymentCreateDto);
+            return Ok();
+        }
+    }
+}
