eeed0ab91324ca8258e1245e1067ccad3dbd373c
Luis J. Montes <luisjavm3@gmail.com>
2022-09-30 13:08:03 -0500

    Complete InsertLoan implementation

--- Cobros.API/Core/Business/LoanBusiness.cs
+++ Cobros.API/Core/Business/LoanBusiness.cs
@@ -51,29 +51,68 @@ namespace Cobros.API.Core.Business
 
         public async Task InsertLoan(int cobroId, LoanCreateDto loanCreateDto)
         {
-            var existingCobro = await _unitOfWork.Cobros.GetByIdAsync(cobroId);
+            var existingCobro = await _unitOfWork.Cobros.GetByIdWithLoansAsync(cobroId);
 
             if (existingCobro == null)
                 throw new AppException($"Cobro with Id: {cobroId} not found.");
 
+            if (loanCreateDto.RoutePosition > existingCobro.Loans.Count() + 1)
+                throw new AppException("RoutePosition must be less than or equal to the total of active loans in Cobro + 1.");
+
             // Restrict user
             if (User.Role == Role.USER)
                 if (!User.CobroIds.Contains(cobroId))
-                    throw new AccessForbiddenException("Access Forbidden");
+                    throw new AccessForbiddenException("Access Forbidden.");
 
             // Check Customer existence.
-            var existingCustomer = await _unitOfWork.Customers.GetByIdAsync(loanCreateDto.CustomerId);
+            var existingCustomer = await _unitOfWork.Customers.GetByIdWithActiveLoan(loanCreateDto.CustomerId);
 
             if (existingCustomer == null)
                 throw new AppException($"Customer with Id: {loanCreateDto.CustomerId} does not exist.");
 
+            // Customer cannot get more than one active loan.
+            if (existingCustomer.Loans.Count() > 0)
+                throw new AppException($"Customer has an active loan with LoanId: {existingCustomer.Loans.First().Id}");
+
             var toInsert = _mapper.Map<Loan>(loanCreateDto);
             toInsert.CobroId = cobroId;
+            
+            // Insert in the last route position plus one - the easiest way.
+            if(loanCreateDto.RoutePosition == existingCobro.Loans.Count() + 1)
+            {
+                await _unitOfWork.Loans.InsertAsync(toInsert);
+                await _unitOfWork.CompleteAsync();
+                return;
+            }
 
-            // More logic here
+            // Sort route when toInsert-loan position is less than Cobro total loans.
+            try
+            {
+                _unitOfWork.BeginTransaccion();
 
-            await _unitOfWork.Loans.InsertAsync(toInsert);
-            await _unitOfWork.CompleteAsync();
+                // TOTAL - POSITION + 1
+                int iterations = existingCobro.Loans.Count() - loanCreateDto.RoutePosition + 1;
+                int position = existingCobro.Loans.Count();
+
+                for (int i = 0; i < iterations; i++)
+                {
+                    var loan = existingCobro.Loans.FirstOrDefault(l => l.RoutePosition == position);
+                    loan.RoutePosition = position + 1;
+                    _unitOfWork.Loans.Update(loan);
+                    await _unitOfWork.CompleteAsync();
+                    position--;
+                }
+
+                await _unitOfWork.Loans.InsertAsync(toInsert);
+                await _unitOfWork.CompleteAsync();
+
+                _unitOfWork.Commit();
+            }
+            catch (Exception)
+            {
+                _unitOfWork.Rollback();
+                throw new AppException("Cannot insert loan.");
+            }
         }
     }
 }
