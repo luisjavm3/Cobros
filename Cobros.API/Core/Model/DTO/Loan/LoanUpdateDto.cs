b0cb9870054d15bb618be00504ee38c9af1c42c7
Luis J. Montes <luisjavm3@gmail.com>
2022-10-19 17:38:16 -0500

    Create LoanUpdateDto

---
+++ Cobros.API/Core/Model/DTO/Loan/LoanUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cobros.API.Core.Model.DTO.Loan
+{
+    public class LoanUpdateDto
+    {
+        [Required(ErrorMessage ="RoutePosition is required.")]
+        [Range(1, int.MaxValue, ErrorMessage ="RoutePosition must be greater than 0.")]
+        public int RoutePosition { get; set; }
+    }
+}
